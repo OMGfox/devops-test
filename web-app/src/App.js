c7da41a1aa0655e6579b74aa01301023e0f58eb4
ZinovevAI <zin42152@gmail.com>
2023-02-12 21:20:30 +0300

    1. Добавлена папка web-app 2. Исправлена опечатка в пути к папке с web-app

---
+++ web-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+	<h1>Test of revert</h1>
+        <h2>
+          Ansible
+        </h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
